dbae27b15092bb5c6e3332ac5a1306f2944c2224
karnamin <aminkarn18@gmail.com>
2019-04-17 18:09:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
 
-class App extends Component {
-  render() {
+const App = () => {
+    const APP_ID = "c7a31472";
+    const APP_KEY = "2900039fca74f3e0d33bd142a898eaa0";
+
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("");
+    const [query, setQuery] = useState("chicken");
+
+    useEffect(() => {
+        getRecipes();
+    }, [query]);
+
+    const getRecipes = async () => {
+        const response = await fetch(
+            `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+        );
+        const data = await response.json();
+        setRecipes(data.hits);
+        console.log(data.hits);
+    };
+
+    const updateSearch = e => {
+        setSearch(e.target.value);
+    };
+
+    const getSearch = e => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch("");
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <form className="search-form" onSubmit={getSearch}>
+                <input
+                    className="search-bar"
+                    type="text"
+                    value={search}
+                    onChange={updateSearch}
+                />
+                <button className="search-button" type="submit">
+                    Search
+                </button>
+            </form>
+            <div className="recipes">
+                {recipes.map(recipe => (
+                    <Recipe
+                        key={recipe.recipe.label}
+                        title={recipe.recipe.label}
+                        calories={recipe.recipe.calories}
+                        image={recipe.recipe.image}
+                        ingredients={recipe.recipe.ingredients}
+                    />
+                ))}
+            </div>
+        </div>
     );
-  }
-}
+};
 
 export default App;
